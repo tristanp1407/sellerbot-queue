61218c48ca17d72ce79a17449c52615a70fb90b3
Tristan Plet <tristan.plet@octopusev.com>
2023-05-11 08:41:42 +0100

    Add Redis URL

--- src/utils/initialiseQueue.ts
+++ src/utils/initialiseQueue.ts
@@ -1,3 +1,5 @@
 import Queue, { Queue as IQueue } from "bull";
 
-export const userQueue: IQueue = new Queue("userQueue");
+const REDIS_URL = process.env.REDIS_URL || "redis://127.0.0.1:6379";
+
+export const userQueue: IQueue = new Queue("userQueue", REDIS_URL);
