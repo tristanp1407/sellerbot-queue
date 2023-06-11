ee29889827e056568fdf831f342a34c6154e6d01
Tristan Plet <tristan.plet@octopusev.com>
2023-06-11 12:25:24 +0100

    Log job timestamp

--- src/workers/followUser.ts
+++ src/workers/followUser.ts
@@ -11,6 +11,8 @@ export const followUser = async (job) => {
   const jobTimestamp = job.queue.delayedTimestamp;
   const currentTimestamp = new Date().getTime();
 
+  console.log(jobTimestamp);
+
   // Check if job's timestamp is older than 30 seconds
   if (currentTimestamp - jobTimestamp > 30000) {
     console.log("Job's timestamp is older than 30 seconds, aborting.");
